0b2f806c3a4141db35412c53af47c8d0bb08304e
EsmeraldaQuintero <esmeralda.quintero.2016@outlook.com>
2016-08-18 09:47:21 -0700

    Stream listo

--- Prueba de stream/MainWindow.xaml.cs
+++ Prueba de stream/MainWindow.xaml.cs
@@ -10,12 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.IO;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using Emgu.CV;
+
+
 
 namespace Prueba_de_stream
 {
@@ -27,7 +31,7 @@ namespace Prueba_de_stream
         private Capture _capture = null;
         private bool _captureInProgress;
         int CameraDevice = 0;
-        Image<Bgr, Byte> currentImage;
+        Image<Bgr, Byte> currentFrame;
         
         public MainWindow()
         {
@@ -35,14 +39,18 @@ namespace Prueba_de_stream
 
             CvInvoke.UseOpenCL = false;
 
-            try
+            if (_capture == null)   //if camera capture hasn't been created, then created one
             {
-                _capture = new Capture();
-                _capture.ImageGrabbed += ProcessFrame;
-            }
-            catch (NullReferenceException excpt)
-            {
-                MessageBox.Show(excpt.Message);
+                try
+                {   //Creating the camera capture
+                    //_capture = new Capture("http://192.168.1.65/mjpg/video.mjpg");
+                    _capture = new Capture();
+                    _capture.ImageGrabbed += ProcessFrame;
+                }
+                catch (NullReferenceException excpt)
+                {   //show errors if there is any
+                    MessageBox.Show(excpt.Message);
+                }
             }
 
             captureButton.Content = "Start Capture";
@@ -50,13 +58,12 @@ namespace Prueba_de_stream
 
         private void captureButtonClick(object sender, EventArgs e)
         {
-            if (_capture != null)
+            if (_capture != null)   //if camera capture has been successfully created
             {
                 if (_captureInProgress)
                 {  //stop the capture
                     captureButton.Content = "Start Capture";
                     _capture.Pause();
-                    updateImage();
                 }
                 else
                 {   //start the capture
@@ -68,18 +75,45 @@ namespace Prueba_de_stream
             }
         }
 
-        private void ProcessFrame(object sender, EventArgs arg)
+
+        private void ProcessFrame(object sender, EventArgs e)
         {
-            Mat frame = new Mat();
-            _capture.Retrieve(frame, CameraDevice);
+            Mat newFrame = new Mat();
+            _capture.Retrieve(newFrame);
+            currentFrame = newFrame.ToImage<Bgr, Byte>();
 
-            currentImage = frame.ToImage<Bgr,Byte>();
 
+            var processFrame = newFrame.ToImage<Gray, Byte>();
+            //var processFrame = skinDetection.YCrCbDetectSkin(newFrame, YCrCb_min, YCrCb_max);
+            //DetectGesture(processFrame);
+            DisplayImage(currentFrame, processFrame);
         }
 
-        private void updateImage()
+
+        private void DisplayImage(Image<Bgr, Byte> currentFrame, Image<Gray, Byte> processFrame)
         {
-            image1.Source = BitmapSourceConvert.ToBitmapSource(currentImage);
+            Dispatcher.Invoke(new Action(() =>
+            {
+                sourceImage.Source = ToImageSource(currentFrame.Bitmap);
+
+               // motionImage.Source = ToImageSource(processFrame.Bitmap);
+            }));
+        }
+
+        private BitmapImage ToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
         }
 
     }
