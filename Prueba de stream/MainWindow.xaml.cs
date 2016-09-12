2cb1638737a1d7569af7bcc932d6ce7e8e80a000
EsmeraldaQuintero <esmeralda.quintero.2016@outlook.com>
2016-09-12 11:50:43 -0700

    ready to erode

--- Prueba de stream/MainWindow.xaml.cs
+++ Prueba de stream/MainWindow.xaml.cs
@@ -8,25 +8,117 @@ using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using Emgu.CV;
 using Emgu.CV.Cuda;
-
-
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Prueba_de_stream
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private Capture _capture = null;
         private bool _captureInProgress;
-        int CameraDevice = 0;
-        long matchTime;
+
+        public int MinHue
+        {
+            get
+            {
+                return _minHue;
+            }
+            set
+            {
+                _minHue = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int MaxHue
+        {
+            get
+            {
+                return _maxHue;
+            }
+            set
+            {
+                _maxHue = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public int MinBrig
+        {
+            get
+            {
+                return _minBrig;
+            }
+            set
+            {
+                _minBrig = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public int MaxBrig
+        {
+            get
+            {
+                return _maxBrig;
+            }
+            set
+            {
+                _maxBrig = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public int MaxSat
+        {
+            get
+            {
+                return _maxSat;
+            }
+            set
+            {
+                _maxSat = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int MinSat
+        {
+            get
+            {
+                return _minSat;
+            }
+            set
+            {
+                _minSat = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int _minHue = 0;
+        public int _maxHue = 0;
+        public int _minBrig = 0;
+        public int _maxBrig = 0;
+        private int _maxSat = 0;
+        private int _minSat = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
 
+            DataContext = this;
             CvInvoke.UseOpenCL = false;
 
             if (_capture == null)   //if camera capture hasn't been created, then created one
@@ -68,16 +160,42 @@ namespace Prueba_de_stream
 
         private void ProcessFrame(object sender, EventArgs e)
         {
+            long matchTime;
+            int TRAIN_WIDTH = 400;
+            int TRAIN_HEIGHT = 300;
+
             Mat newFrame = new Mat();
             _capture.Retrieve(newFrame);
-            Mat newModelFrame = CvInvoke.Imread("C:\\Users\\uabc\\Documents\\EmgucvWPF\\Prueba de stream\\Img1.jpg", LoadImageType.Grayscale);
+            //Mat newFrame = CvInvoke.Imread("C:\\Users\\uabc\\Documents\\EmgucvWPF\\Prueba de stream\\Img1.jpg", LoadImageType.Grayscale);
+            Mat newModelFrame = CvInvoke.Imread("C:\\Users\\uabc\\Documents\\EmgucvWPF\\Prueba de stream\\Img4.jpg", LoadImageType.Grayscale);
 
-            var currentFrame = newFrame.ToImage<Gray, Byte>();
-            var processFrame = newModelFrame.ToImage<Gray, Byte>();
+            //var currentFrame = newFrame.ToImage<Gray, Byte>().ThresholdToZero(new Gray(5.0)).Canny(80,150);
+
+            var currentFrame = newFrame.ToImage<Gray, Byte>().Resize(TRAIN_WIDTH, TRAIN_HEIGHT, Emgu.CV.CvEnum.Inter.Cubic);
+            var processFrame = newModelFrame.ToImage<Gray, Byte>().Resize(TRAIN_WIDTH, TRAIN_HEIGHT, Emgu.CV.CvEnum.Inter.Cubic);
+
+
+
+            var image = (newFrame.ToImage<Bgr, byte>().Resize(TRAIN_WIDTH, TRAIN_HEIGHT, Emgu.CV.CvEnum.Inter.Cubic)).Convert<Hsv, byte>();
+            Image<Gray, byte>[] channels = image.Split();
+            var ch0 = channels[0];  //matiz
+
+            var ch1 = channels[1];  //saturacion
+            var ch2 = channels[2];  //brillo
+            Image<Gray, byte> huefilter = ch0.InRange(new Gray(_minHue), new Gray(_maxHue));
+            Image<Gray, byte> saturfilter = ch1.InRange(new Gray(_minSat), new Gray(_maxSat));
+            Image<Gray, byte> brightnessFilter = ch2.InRange(new Gray(_minBrig), new Gray(_maxBrig));
+            Image<Gray, byte> result2 = huefilter.And(saturfilter);
+            Image<Gray, byte> result3 = result2.And(brightnessFilter);
+            //currentFrame = huefilter;
+            currentFrame = result3;
+
+
+
             DisplayImage(currentFrame, processFrame);
 
-            using (Mat modelImage = currentFrame.Mat)
-            using (Mat observedImage = processFrame.Mat)
+            using (Mat modelImage = processFrame.Mat)
+            using (Mat observedImage = currentFrame.Mat)
             {
                 Mat result = DrawMatches.Draw(modelImage, observedImage, out matchTime);
                 var resultFrame = result.ToImage<Bgr, Byte>();
@@ -118,6 +236,5 @@ namespace Prueba_de_stream
                 return bitmapimage;
             }
         }
-
     }
 }
