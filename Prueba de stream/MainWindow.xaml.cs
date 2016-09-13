5c942b387c6d3e6590625d13580e60330190b4d7
EsmeraldaQuintero <esmeralda.quintero.2016@outlook.com>
2016-09-13 11:05:53 -0700

    Segmentation improvement

--- Prueba de stream/MainWindow.xaml.cs
+++ Prueba de stream/MainWindow.xaml.cs
@@ -21,117 +21,143 @@ namespace Prueba_de_stream
         private Capture _capture = null;
         private bool _captureInProgress;
 
-        public int MinHue
+        public int Hue1
         {
             get
             {
-                return _minHue;
+                return _hue1;
             }
             set
             {
-                _minHue = value;
+                _hue1 = value;
                 NotifyPropertyChanged();
             }
         }
-        public int MaxHue
+        public int Hue2
         {
             get
             {
-                return _maxHue;
+                return _hue2;
             }
             set
             {
-                _maxHue = value;
+                _hue2 = value;
                 NotifyPropertyChanged();
             }
         }
+        private int _hue1 = 59;
+        private int _hue2 = 26;
+        private int _maxHue = 128;
 
-        public int MinBrig
+        public int Sat1
         {
             get
             {
-                return _minBrig;
+                return _sat1;
             }
             set
             {
-                _minBrig = value;
+                _sat1 = value;
                 NotifyPropertyChanged();
             }
         }
-
-        public int MaxBrig
+        public int Sat2
         {
             get
             {
-                return _maxBrig;
+                return _sat2;
             }
             set
             {
-                _maxBrig = value;
+                _sat2 = value;
                 NotifyPropertyChanged();
             }
         }
-
-        public int MaxSat
-        {
-            get
-            {
-                return _maxSat;
-            }
-            set
-            {
-                _maxSat = value;
-                NotifyPropertyChanged();
-            }
-        }
-        public int MinSat
-        {
-            get
-            {
-                return _minSat;
-            }
-            set
-            {
-                _minSat = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        public int MinSqrMin
-        {
-            get
-            {
-                return _minSqrMin;
-            }
-            set
-            {
-                _minSqrMin = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        public int MinSqrMax
-        {
-            get
-            {
-                return _minSqrMax;
-            }
-            set
-            {
-                _minSqrMax = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        private int _minHue = 79;
-        public int _maxHue = 128;
-        public int _minBrig = 0;
-        public int _maxBrig = 255;
+        private int _sat1 = 42;
+        private int _sat2 = 43;
         private int _maxSat = 128;
-        private int _minSat = 28;
-        private int _minSqrMin = 5;
-        private int _minSqrMax = 2;
 
+        public int Brig1
+        {
+            get
+            {
+                return _brig1;
+            }
+            set
+            {
+                _brig1 = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int Brig2
+        {
+            get
+            {
+                return _brig2;
+            }
+            set
+            {
+                _brig2 = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _brig1 = 0;
+        private int _brig2 = 0;
+        private int _maxBrig = 255;
+
+        public int Erode1
+        {
+            get
+            {
+                return _erode1;
+            }
+            set
+            {
+                _erode1 = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int Erode2
+        {
+            get
+            {
+                return _erode2;
+            }
+            set
+            {
+                _erode2 = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _erode1 = 2;
+        private int _erode2 = 2;
+
+        public int Dilate1
+        {
+            get
+            {
+                return _dilate1;
+            }
+            set
+            {
+                _dilate1 = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int Dilate2
+        {
+            get
+            {
+                return _dilate2;
+            }
+            set
+            {
+                _dilate2 = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _dilate1 = 6;
+        private int _dilate2 = 8;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -193,66 +219,70 @@ namespace Prueba_de_stream
             int TRAIN_WIDTH = 400;
             int TRAIN_HEIGHT = 300;
 
+            //Mat newFrame = CvInvoke.Imread("C:\\Users\\uabc\\Documents\\EmgucvWPF\\Prueba de stream\\Img1.jpg", LoadImageType.Grayscale);
+            //originalCurrentFrame._EqualizeHist();      //contraste
+            //originalCurrentFrame._GammaCorrect(1.4d);
+
             Mat newFrame = new Mat();
             _capture.Retrieve(newFrame);
-            //Mat newFrame = CvInvoke.Imread("C:\\Users\\uabc\\Documents\\EmgucvWPF\\Prueba de stream\\Img1.jpg", LoadImageType.Grayscale);
             Mat newModelFrame = CvInvoke.Imread("C:\\Users\\uabc\\Documents\\EmgucvWPF\\Prueba de stream\\Img6.jpg", LoadImageType.Grayscale);
 
             var currentFrame = (newFrame.ToImage<Bgr, byte>().Resize(TRAIN_WIDTH, TRAIN_HEIGHT, Emgu.CV.CvEnum.Inter.Cubic)).Convert<Hsv, byte>();
             var processFrame = (newModelFrame.ToImage<Bgr, byte>().Resize(TRAIN_WIDTH, TRAIN_HEIGHT, Emgu.CV.CvEnum.Inter.Cubic)).Convert<Hsv, byte>();
 
+            var currentFrameWithErode = ColorSegmentation(currentFrame, _hue1, _sat1, _brig1);
+            var currentFrameWithDilate = ColorSegmentation(currentFrame, _hue2, _sat2, _brig2);
 
-            currentFrame._EqualizeHist();
-            currentFrame._GammaCorrect(1.4d);
+            currentFrameWithErode = Morphology(currentFrameWithErode,_dilate1, _erode1, true);
+            currentFrameWithDilate = Morphology(currentFrameWithDilate, _dilate2, _erode2, false);
+
+            var subFrame = currentFrameWithDilate.Sub(currentFrameWithErode);
+
+            DisplayImage(currentFrame, currentFrameWithErode, currentFrameWithDilate, subFrame);
+
+            DisplayResult(currentFrameWithDilate.Sub(currentFrameWithErode), 1000);
+
+
+            //using (Mat modelImage = processFrame.Mat)
+            //using (Mat observedImage = currentFrame.Mat)
+            //{
+            //    Mat result = DrawMatches.Draw(modelImage, observedImage, out matchTime);
+            //    var resultFrame = result.ToImage<Hsv, Byte>();
+            //    DisplayResult(resultFrame, matchTime);
+            //}
+        }
+
+        private Image<Gray, byte> Morphology(Image<Gray, byte> image, int dilateSize, int erodeSize, bool erode)
+        {
+            var dilateImage = image.Copy(image);
+
+            Mat rec_Erode = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new System.Drawing.Size(erodeSize, erodeSize), new System.Drawing.Point(erodeSize / 2, erodeSize / 2));
+            Mat rec_Dilate = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new System.Drawing.Size(dilateSize, dilateSize), new System.Drawing.Point(dilateSize / 2, dilateSize / 2));
+
+            CvInvoke.Dilate(dilateImage, dilateImage, rec_Dilate, new System.Drawing.Point(1, 1), 2, BorderType.Default, new MCvScalar(0, 0, 0));
+            if (erode)
+            {
+                CvInvoke.Erode(dilateImage, dilateImage, rec_Erode, new System.Drawing.Point(1, 1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
+            }
+            return dilateImage;
+        }
+
+        private Image<Gray,byte> ColorSegmentation(Image<Hsv, byte> currentFrame, int hue, int sat, int brig)
+        {
             Image<Gray, byte>[] channels = currentFrame.Split();
             var ch0 = channels[0];  //matiz
             var ch1 = channels[1];  //saturacion
             var ch2 = channels[2];  //brillo
-            Image<Gray, byte> huefilter = ch0.InRange(new Gray(_minHue), new Gray(_maxHue));
-            Image<Gray, byte> saturfilter = ch1.InRange(new Gray(_minSat), new Gray(_maxSat));
-            Image<Gray, byte> brightnessFilter = ch2.InRange(new Gray(_minBrig), new Gray(_maxBrig));
+            Image<Gray, byte> huefilter = ch0.InRange(new Gray(hue), new Gray(_maxHue));
+            Image<Gray, byte> saturfilter = ch1.InRange(new Gray(sat), new Gray(_maxSat));
+            Image<Gray, byte> brightnessFilter = ch2.InRange(new Gray(brig), new Gray(_maxBrig));
             Image<Gray, byte> result2 = huefilter.And(saturfilter);
             Image<Gray, byte> result3 = result2.And(brightnessFilter);
 
-            var currentFrameWithFilters = result3;
-
-
-
-            //Mat rect_12 = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new System.Drawing.Size(_minSqrMax, _minSqrMax), new System.Drawing.Point(_minSqrMax / 2, _minSqrMax / 2));
-            //Mat rect_6 = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new System.Drawing.Size(_minSqrMin, _minSqrMin), new System.Drawing.Point(_minSqrMin / 2, _minSqrMin / 2));
-            //CvInvoke.Erode(currentFrame2, currentFrame2, rect_12, new System.Drawing.Point(1, 1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
-            //CvInvoke.Dilate(currentFrame2, currentFrame2, rect_6, new System.Drawing.Point(1, 1), 2, BorderType.Default, new MCvScalar(0, 0, 0));
-
-
-            DisplayImage(currentFrame, currentFrameWithFilters);
-
-            using (Mat modelImage = processFrame.Mat)
-            using (Mat observedImage = currentFrame.Mat)
-            {
-                Mat result = DrawMatches.Draw(modelImage, observedImage, out matchTime);
-                var resultFrame = result.ToImage<Hsv, Byte>();
-                DisplayResult(resultFrame, matchTime);
-            }
+            return result3;
         }
 
-
-        //public Image<Gray, Byte> YCrCbDetectSkinCpu(Mat img, IColor min, IColor max)
-        //{
-        //    Mat yccMat = new Mat();
-        //    var skin = new Image<Gray, Byte>(img.Width, img.Height);
-        //    CvInvoke.CvtColor(img, yccMat, ColorConversion.Bgr2YCrCb);
-
-        //    // mejorar esta linea
-        //    skin = yccMat.ToImage<Ycc, Byte>().InRange((Ycc)min, (Ycc)max);
-
-        //    Mat rect_12 = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new System.Drawing.Size(12, 12), new System.Drawing.Point(6, 6));
-        //    Mat rect_6 = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new System.Drawing.Size(6, 6), new System.Drawing.Point(3, 3));
-        //    CvInvoke.Erode(skin, skin, rect_12, new System.Drawing.Point(6, 6), 1, BorderType.Default, new MCvScalar(0, 0, 0));
-        //    CvInvoke.Dilate(skin, skin, rect_6, new System.Drawing.Point(3, 3), 2, BorderType.Default, new MCvScalar(0, 0, 0));
-
-        //    return skin;
-        //}
-        private void DisplayResult(Image<Hsv, Byte> resultFrame, long matchTime)
+        private void DisplayResult(Image<Gray, Byte> resultFrame, long matchTime)
         {
             Dispatcher.Invoke(new Action(() =>
             {
@@ -261,12 +291,14 @@ namespace Prueba_de_stream
             }));
         }
 
-        private void DisplayImage(Image<Hsv, Byte> currentFrame, Image<Gray, Byte> processFrame)
+        private void DisplayImage(Image<Hsv, Byte> currentFrame, Image<Gray, Byte> minFrame, Image<Gray, Byte> maxFrame, Image<Gray, Byte> subFrame)
         {
             Dispatcher.Invoke(new Action(() =>
             {
                 sourceImage.Source = ToImageSource(currentFrame.Bitmap);
-                modelImage.Source = ToImageSource(processFrame.Bitmap);
+                filterImage.Source = ToImageSource(minFrame.Bitmap);
+                erodeImage.Source = ToImageSource(maxFrame.Bitmap);
+                dilateImage.Source = ToImageSource(subFrame.Bitmap);
             }));
         }
 
