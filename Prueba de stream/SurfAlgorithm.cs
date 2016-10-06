a0b369d722ad83b59e9dec58996dceb69ba0a8c9
EsmeraldaQuintero <esmeralda.quintero.2016@outlook.com>
2016-10-06 13:39:34 -0700

    Without match

--- Prueba de stream/SurfAlgorithm.cs
+++ Prueba de stream/SurfAlgorithm.cs
@@ -12,18 +12,24 @@ namespace Prueba_de_stream
 {
     public class SurfAlgorithm
     {
+
+        private const int   K = 2;
+        private const int   START_IDX = 0;
+        private const int   CHANNEL_MONO = 1;
+        private const int   RANSAC_THRESH = 2;
+        private const int   ROTATION_BINS = 20;
+        private const int   MIN_VALID_MATCHES = 8;
+        private const byte  VALID_MATCH_VAL = 1;
+        private const byte  NOT_VALID_MATCH_VAL = 0;
         private const double MATCH_THRESH = 0.6;
+        private const double SCALE_INCREMENT = 1.5;
+        private const double UNIQUENESS_THRESHOLD = 0.8;
         private const double HESSIAN_THRESH = 300;
-        private const bool IGNORE_PROVIDED_KEYPOINS = false;
-        private const int K = 2;
-        private const int START_IDX = 0;
-        private const byte VALID_MATCH_VAL = 1;
-        private const byte NOT_VALID_MATCH_VAL = 0;
+        private const bool  IGNORE_PROVIDED_KEYPOINS = false;
 
         private static SURF surfCPU;
         static SurfAlgorithm()
         {
-
             surfCPU = new SURF(HESSIAN_THRESH);
         }
 
@@ -42,9 +48,13 @@ namespace Prueba_de_stream
             SurfImage modelWeapon = GetSurfFeaturesOf(modelWeaponImage);
             SurfImage observedCamera = GetSurfFeaturesOf(observedCameraImage);
             VectorOfVectorOfDMatch matches = GetMatches(modelWeapon, observedCamera);
-            double ssdAcumulator = CalculateMatch(matches);
-
-            return (ssdAcumulator>MATCH_THRESH);
+            Mat matchesMask = new Mat(matches.Size, 1, DepthType.Cv8U, CHANNEL_MONO);
+            Mat homography = null;
+            if ( GetMatchesMask(matches, matchesMask) )
+            {
+                homography = GetHomography(modelWeapon,observedCamera,matches, matchesMask);
+            }
+            return (homography!=null);
         }
 
         private static VectorOfVectorOfDMatch GetMatches(SurfImage modelWeapon, SurfImage observedCamera)
@@ -56,13 +66,15 @@ namespace Prueba_de_stream
             return matches;
         }
 
-        private static Mat CalculateMatch(VectorOfVectorOfDMatch matches)
+        private static bool GetMatchesMask(VectorOfVectorOfDMatch matches, Mat mask)
         {
-            MDMatch[][] listOfMatches = matches.ToArrayOfArray();
+            MDMatch[][] arrayOfMatches = matches.ToArrayOfArray();
             byte[] data = new byte[matches.Size];
+            double SSDacumulator = 0.0;
             for (int i = 0; i < matches.Size; i++)
             {
-                if( ValidSumSqrDiff(listOfMatches[i][0].Distance, listOfMatches[i][1].Distance))
+                SSDacumulator += diffSqr(arrayOfMatches[i][0].Distance, arrayOfMatches[i][1].Distance);
+                if (ValidDistance(arrayOfMatches[i][0].Distance, arrayOfMatches[i][1].Distance))
                 {
                     data[i] = VALID_MATCH_VAL;
                 }
@@ -70,17 +82,37 @@ namespace Prueba_de_stream
                 {
                     data[i] = NOT_VALID_MATCH_VAL;
                 }
-
             }
-            Mat mask = new Mat();
             Marshal.Copy(data, START_IDX, mask.DataPointer, matches.Size);
-            return mask;
+            return (SSDacumulator > MATCH_THRESH);
         }
 
-        private static bool ValidSumSqrDiff(float distance1, float distance2)
+        private static double diffSqr(float distance1, float distance2)
         {
-            double result = (distance1 - distance2) * (distance1 - distance2);
-            return (result > MATCH_THRESH);
+            return (distance1 - distance2) * (distance1 - distance2);
+        }
+
+        private static bool ValidDistance(float distance1, float distance2)
+        {
+            return ((distance1/distance2) > UNIQUENESS_THRESHOLD);
+        }
+
+        private static Mat GetHomography(SurfImage modelWeapon, SurfImage observedCamera, VectorOfVectorOfDMatch matches, Mat mask)
+        {
+            Mat homography = null;
+            int nonZeroCountMatches = CvInvoke.CountNonZero(mask);
+            if (nonZeroCountMatches >= matches.Size/2)
+            {
+                nonZeroCountMatches = Features2DToolbox.VoteForSizeAndOrientation(modelWeapon.keyPoints, observedCamera.keyPoints,
+                   matches, mask, SCALE_INCREMENT, ROTATION_BINS);
+                if (nonZeroCountMatches >= MIN_VALID_MATCHES)
+                {
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelWeapon.keyPoints, observedCamera.keyPoints, matches, mask, RANSAC_THRESH);
+                    nonZeroCountMatches = CvInvoke.CountNonZero(mask);
+                    return (nonZeroCountMatches >= MIN_VALID_MATCHES) ? homography : null;
+                }
+            }
+            return null;
         }
     }
 }
